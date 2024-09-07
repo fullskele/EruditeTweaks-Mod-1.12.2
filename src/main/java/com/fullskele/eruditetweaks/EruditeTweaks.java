1197a8c2455488cf06acd12a710221fc7553057c
fullskele <jacobmajosi@gmail.com>
2024-09-07 13:38:09 -0400

    bump version
    v1.0.1

--- src/main/java/com/fullskele/eruditetweaks/EruditeTweaks.java
+++ src/main/java/com/fullskele/eruditetweaks/EruditeTweaks.java
@@ -16,7 +16,7 @@ import java.io.File;
 public class EruditeTweaks {
     public static final String MODID = "eruditetweaks";
     public static final String NAME = "Erudite Tweaks";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
 
     public static File config;
